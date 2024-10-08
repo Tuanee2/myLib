9fb86826050ae256fc222d00aba80d7cf1cc49a0
Tuanee2 <142774083+Tuanee2@users.noreply.github.com>
2024-10-08 13:54:10 +0700

    Xóa file matrix.xcodeproj

---
+++ matrix.cpp
@@ -0,0 +1,8 @@
+//
+//  matrix.cpp
+//  matrix
+//
+//  Created by tuan on 8/8/24.
+//
+
+#include "matrix.hpp"
---
+++ matrix.hpp
@@ -0,0 +1,13 @@
+//
+//  matrix.hpp
+//  matrix
+//
+//  Created by tuan on 8/8/24.
+//
+
+#ifndef matrix_hpp
+#define matrix_hpp
+
+#include <stdio.h>
+
+#endif /* matrix_hpp */
